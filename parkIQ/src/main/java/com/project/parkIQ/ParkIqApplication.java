ecec819b804403bea857cffaf91c185d44b3f3ee
dhivya-1010 <dhivya.v2024cse@sece.ac.in>
2026-06-03 21:07:04 +0530

    project initiated

---
+++ parkIQ/src/main/java/com/project/parkIQ/ParkIqApplication.java
@@ -0,0 +1,13 @@
+package com.project.parkIQ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkIqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkIqApplication.class, args);
+	}
+
+}
