4394ddab2dd800dc19963c7a8bfad1c0d03c4204
dhivya-1010 <dhivya.v2024cse@sece.ac.in>
2026-06-11 17:32:25 +0530

    User entity and repo created

--- parkIQ/src/main/java/com/project/parkIQ/ParkIqApplication.java
+++ parkIQ/src/main/java/com/project/parkIQ/ParkIqApplication.java
@@ -7,12 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ParkIqApplication {
 
 	public static void main(String[] args) {
-
-<<<<<<< HEAD
-=======
-
-
->>>>>>> 128eb96a817c80c8f6d41e55343931428489547b
         SpringApplication.run(ParkIqApplication.class, args);
 	}
 
