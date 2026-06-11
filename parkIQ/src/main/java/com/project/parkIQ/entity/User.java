af501be240c8d5f84fad85f017a5eeb304793638
dhivya-1010 <dhivya.v2024cse@sece.ac.in>
2026-06-11 18:06:19 +0530

    Service layer for authentication

--- parkIQ/src/main/java/com/project/parkIQ/entity/User.java
+++ parkIQ/src/main/java/com/project/parkIQ/entity/User.java
@@ -1,11 +1,9 @@
 package com.project.parkIQ.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name="users")
 public class User {
 
     @Id
